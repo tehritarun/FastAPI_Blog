c8a9468fae54ea9f89535b5ac3e1e01bf5e0db91
Tarun Tehri <tehritarun@gmail.com>
2026-02-02 21:43:58 +0530

    Added post route and placeholder posts

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get("/")
+posts = [
+    {
+        "id": "1",
+        "author": "Tarun Tehri",
+        "title": "Dummy post",
+        "content": "fast api framework",
+        "date_posted": "Feb 1, 2026",
+    },
+    {
+        "id": "2",
+        "author": "divya Tehri",
+        "title": "Another dummy post",
+        "content": "fast api framework",
+        "date_posted": "Feb 2, 2026",
+    },
+]
+
+
+@app.get("/", response_class=HTMLResponse, include_in_schema=False)
+@app.get("/posts", response_class=HTMLResponse, include_in_schema=False)
 def home():
-    return {"message": "hello world!"}
+    return f"<h1>{posts[0]['title']}<h1>"
+
+
+@app.get("/api/posts")
+def get_posts():
+    return posts
